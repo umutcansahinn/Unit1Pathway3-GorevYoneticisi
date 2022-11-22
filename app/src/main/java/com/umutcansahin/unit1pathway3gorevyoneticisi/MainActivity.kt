cd359baa3e34f6156d3df6ff3594a09ce8d1cbc3
umut <umutcan199726@gmail.com>
2022-11-22 03:41:10 +0300

    app

---
+++ app/src/main/java/com/umutcansahin/unit1pathway3gorevyoneticisi/MainActivity.kt
@@ -0,0 +1,84 @@
+package com.umutcansahin.unit1pathway3gorevyoneticisi
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.umutcansahin.unit1pathway3gorevyoneticisi.ui.theme.Unit1Pathway3GorevYoneticisiTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Unit1Pathway3GorevYoneticisiTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    TaskCompletedScreen()
+                }
+            }
+        }
+    }
+}
+@Composable
+fun TaskCompletedScreen(){
+    TaskCompletedScreenCard(
+        text1 = stringResource(id = R.string.all_task_completed),
+        text2 = stringResource(id = R.string.nice_work),
+        imagePainter = painterResource(id = R.drawable.ic_task_completed)
+    )
+}
+@Composable
+private fun TaskCompletedScreenCard(
+    text1: String,
+    text2: String,
+    imagePainter: Painter
+) {
+    Column (
+        modifier = Modifier
+            .fillMaxWidth()
+            .fillMaxHeight(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+
+    ){
+        Image(painter = imagePainter, contentDescription = null)
+        Text(
+            text = text1,
+            fontWeight = FontWeight.Bold,
+            fontSize = 24.sp,
+            modifier = Modifier
+                .padding(top = 24.dp, bottom = 8.dp)
+        )
+        Text(
+            text = text2,
+            fontSize = 16.sp
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    Unit1Pathway3GorevYoneticisiTheme {
+        Surface {
+            TaskCompletedScreen()
+        }
+    }
+}
